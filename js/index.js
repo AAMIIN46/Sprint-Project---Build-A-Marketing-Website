e1e710c7c036d20b0befa3aa22f62eea99a92437
aamiin46 <you@example.com>
2023-03-19 16:48:08 +0300

    js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@
 
 /* 
 
+
+
 1. Start creating three prompts, the first two for the two numbers and the second one for the operation
 2. Also declare another variable for the result.
 3. Using conditional statement, check if the operation is addition, subtraction, division or multipilcation
@@ -11,3 +13,12 @@
 5. Console.log the result.
 
 */
+const value1 = prompt("entere first nume");
+const value2 = prompt("entere first nume");
+const value3 = prompt("entere first nume");
+if (value1 && value2 && value3) {
+  const result = value1 + value2 * value3;
+  console.log(result);
+} else {
+  console.log("number sax ah gali waala fadlan");
+}
